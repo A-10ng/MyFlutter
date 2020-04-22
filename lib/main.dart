8ac71654d414facc26851b6cf41bfef9edfbc9b8
LongSh1z <anthony.long@jodoinc.com>
2020-04-22 15:28:52 +0800

    function---增加弹性布局练习

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'route/CheckboxDemo.dart';
 import 'route/TextFieldAndFormDemo.dart';
 import 'route/IndicatorDemo.dart';
 import 'route/LinearlayoutDemo.dart';
+import 'route/FlexlayoutDemo.dart';
 
 void main() => runApp(MyApp());
 
@@ -284,6 +285,20 @@ class _MyHomePageState extends State<MyHomePage> {
                       onPressed: () {
                         Navigator.push(context, MaterialPageRoute(builder: (context) {
                           return LinearlayoutDemo();
+                        }));
+                      },
+                      color: Colors.blue,
+                      highlightColor: Colors.blue[700],
+                      colorBrightness: Brightness.dark,
+                      splashColor: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                    ),
+                    RaisedButton(
+                      child: Text("弹性布局"),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) {
+                          return FlexlayoutDemo();
                         }));
                       },
                       color: Colors.blue,
